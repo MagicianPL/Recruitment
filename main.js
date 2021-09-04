098fb429daee33e803c3299683aeb5f75364e443
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-04 21:27:28 +0200

    shorter and better js code

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ const searching = (e) => {
 /* **************************************************************** */
 /*All functions below are functions for sorting rows*/
 
-function sortTableIdentsLowToHigh() {
+function sortLowToHigh(index) {
   let rows, switching, i, x, y, shouldSwitch;
 
   switching = true;
@@ -81,8 +81,8 @@ function sortTableIdentsLowToHigh() {
       shouldSwitch = false;
       /* Get the two elements you want to compare,
           one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[0];
-      y = rows[i + 1].getElementsByTagName("td")[0];
+      x = rows[i].getElementsByTagName("td")[index];
+      y = rows[i + 1].getElementsByTagName("td")[index];
       // Check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         // If so, mark as a switch and break the loop:
@@ -99,7 +99,7 @@ function sortTableIdentsLowToHigh() {
   }
 }
 
-function sortTableIdentsHighToLow() {
+function sortHighToLow(index) {
   let rows, switching, i, x, y, shouldSwitch;
 
   switching = true;
@@ -116,8 +116,8 @@ function sortTableIdentsHighToLow() {
       shouldSwitch = false;
       /* Get the two elements you want to compare,
           one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[0];
-      y = rows[i + 1].getElementsByTagName("td")[0];
+      x = rows[i].getElementsByTagName("td")[index];
+      y = rows[i + 1].getElementsByTagName("td")[index];
       // Check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
         // If so, mark as a switch and break the loop:
@@ -128,216 +128,6 @@ function sortTableIdentsHighToLow() {
     if (shouldSwitch) {
       /* If a switch has been marked, make the switch
           and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTableTitlesLowToHigh() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-    no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-      first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[1];
-      y = rows[i + 1].getElementsByTagName("td")[1];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTableTitlesHighToLow() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-      no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-        first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-          one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[1];
-      y = rows[i + 1].getElementsByTagName("td")[1];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-          and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTableDatesLowToHigh() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-        no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-          first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-            one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[2];
-      y = rows[i + 1].getElementsByTagName("td")[2];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTableDatesHighToLow() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-        no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-          first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-            one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[2];
-      y = rows[i + 1].getElementsByTagName("td")[2];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTablePagesLowToHigh() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-          no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-            first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-              one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[3];
-      y = rows[i + 1].getElementsByTagName("td")[3];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-              and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-
-function sortTablePagesHighToLow() {
-  let rows, switching, i, x, y, shouldSwitch;
-
-  switching = true;
-  /* Make a loop that will continue until
-          no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("tr");
-    /* Loop through all table rows (except the
-            first, which contains table headers): */
-    for (i = 1; i < rows.length - 1; i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-              one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("td")[3];
-      y = rows[i + 1].getElementsByTagName("td")[3];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-              and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
@@ -347,33 +137,33 @@ function sortTablePagesHighToLow() {
 /*Checking values and sorting*/
 const sortingIdents = (e) => {
   if (e.target.value === "Rosnąco") {
-    sortTableIdentsLowToHigh();
+    sortLowToHigh(0);
   } else if (e.target.value === "Malejąco") {
-    sortTableIdentsHighToLow();
+    sortHighToLow(0);
   }
 };
 
 const sortingTitles = (e) => {
   if (e.target.value === "Rosnąco") {
-    sortTableTitlesLowToHigh();
+    sortLowToHigh(1);
   } else if (e.target.value === "Malejąco") {
-    sortTableTitlesHighToLow();
+    sortHighToLow(1);
   }
 };
 
 const sortingDates = (e) => {
   if (e.target.value === "Rosnąco") {
-    sortTableDatesLowToHigh();
+    sortLowToHigh(2);
   } else if (e.target.value === "Malejąco") {
-    sortTableDatesHighToLow();
+    sortHighToLow(2);
   }
 };
 
 const sortingPages = (e) => {
   if (e.target.value === "Rosnąco") {
-    sortTablePagesLowToHigh();
+    sortLowToHigh(3);
   } else if (e.target.value === "Malejąco") {
-    sortTablePagesHighToLow();
+    sortHighToLow(3);
   }
 };
 
